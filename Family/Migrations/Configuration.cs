278a1b276165e481b4507560f3340714ccdd9158
ninjaofdeath30 <ninjaofdeath30@gmail.com>
2016-03-19 15:37:37 -0600

    re made the database using update-database -TargetMigration $InitialCreate and then I redid the seed method to not be dumb.

--- Family/Migrations/Configuration.cs
+++ Family/Migrations/Configuration.cs
@@ -1,19 +1,18 @@
 namespace Family.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-
     internal sealed class Configuration : DbMigrationsConfiguration<Family.Models.ApplicationDbContext>
     {
-        private ApplicationUserManager userManager;
-
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "Family.Models.ApplicationDbContext";
         }
 
@@ -24,20 +23,22 @@ namespace Family.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
-            
-            var appContext = new Models.ApplicationDbContext();
-            List<Models.ApplicationUser> users = new List<Models.ApplicationUser>
+
+            if (!(context.Users.Any(u => u.UserName == "booher.cody@gmail.com")))
             {
-              new Models.ApplicationUser { UserName = "maybooher@gmail.com", PhoneNumber = "8018857662"},
-              new Models.ApplicationUser { UserName = "booher.cody@gmail.com", PhoneNumber = "4804351882"},
-              new Models.ApplicationUser { UserName = "brittbooher@cox.net", PhoneNumber = "4802209995"},
-              new Models.ApplicationUser { UserName = "brockbooher@cox.net", PhoneNumber = "480220614"}
-            };
+                var userStore = new UserStore<Models.ApplicationUser>(context);
+                var userManager = new UserManager<Models.ApplicationUser>(userStore);
+                List<Models.ApplicationUser> users = new List<Models.ApplicationUser>
+                {
+                  new Models.ApplicationUser { UserName = "maybooher@gmail.com", PhoneNumber = "8018857662"},
+                  new Models.ApplicationUser { UserName = "booher.cody@gmail.com", PhoneNumber = "4804351882"},
+                  new Models.ApplicationUser { UserName = "brittbooher@cox.net", PhoneNumber = "4802209995"},
+                  new Models.ApplicationUser { UserName = "brockbooher@cox.net", PhoneNumber = "480220614"}
+                };
 
-            users.ForEach(async u => await userManager.CreateAsync(u, "212Mayday!"));
-
-            context.SaveChanges();
-
+                users.ForEach(async u => await userManager.CreateAsync(u, "212Mayday!"));
+            }
+            
         }
     }
 }
