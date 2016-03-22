522f0c03476a21845dd26ccab69be8f7c38d4357
Cody Booher <codyb@anglepoint.com>
2016-03-21 21:02:15 -0600

    add the full name claim.

--- Family/Models/IdentityModels.cs
+++ Family/Models/IdentityModels.cs
@@ -49,6 +49,7 @@ namespace Family.Models
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            userIdentity.AddClaim(new Claim("FullName", this.FullName));
             // Add custom user claims here
             return userIdentity;
         }
