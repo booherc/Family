76aa07d3ae2fa8679340c773af88e7656b0fd8cc
Cody Booher <codyb@anglepoint.com>
2016-03-21 19:59:10 -0600

    extra fields on identity models

--- Family/Models/IdentityModels.cs
+++ Family/Models/IdentityModels.cs
@@ -3,12 +3,50 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 
 namespace Family.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName { get { return FirstName + " " + LastName; } }
+
+        [Display(Name = "Street Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Address 2")]
+        public string Address2 { get; set; }
+
+        [Display(Name = "State")]
+        [MaxLength(2)]
+        public string State { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "Zipcode")]
+        [MaxLength(5)]
+        public int Zipcode { get; set; }
+
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        public Image Image { get; set; }
+
+        public bool IsArchived { get; set; }
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
