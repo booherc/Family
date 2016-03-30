0c4655956769728405a550d2e6b25c190396e6c3
Cody Booher <codyb@anglepoint.com>
2016-03-29 23:15:33 -0600

    Post Index page. looks good, need to find out why the name isn't showing.

---
+++ Family/Controllers/PostController.cs
@@ -0,0 +1,94 @@
+﻿using Family.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Family.Controllers
+{
+    [Authorize]
+    public class PostController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Post
+        public ActionResult Index()
+        {
+            var allPosts = db.Posts.Where(p => p.PostId > 0);
+
+            return View(allPosts);
+        }
+
+        // GET: Post/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Post/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Post/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
